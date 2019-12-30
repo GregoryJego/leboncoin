e9bc5e3ed474fe77e599e594af6ea79dc982c4bf
Grégory Jégo <gregoryjego@MacBook-Pro-de-Gregory.local>
2019-12-30 19:05:38 +0100

    maj css

--- src/containers/Offer.js
+++ src/containers/Offer.js
@@ -15,7 +15,7 @@ const Offer = props => {
         // "https://localhost:4000/offer/" + id
       );
       setData(response.data);
-      console.log("Voici la réponse : " + Object.keys(response.data));
+      console.log("Voici la réponse : " + Object.keys(response));
     } catch (e) {
       alert("Une erreur s'est produite");
     }
